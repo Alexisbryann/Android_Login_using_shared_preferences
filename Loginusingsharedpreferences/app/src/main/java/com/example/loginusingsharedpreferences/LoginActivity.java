99fcf74cb198d1725e8e5bdfbbb29d6588b78933
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-10-27 14:23:53 +0300

    login form

---
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.loginusingsharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
--- pluralsight-projects-Android-CandyCodedApplication-e57feeb/app/src/main/java/com/pluralsight/app/Candy.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight.app;
-
-public class Candy {
-    public int id;
-    public String name;
-    public String image;
-    public String price;
-    public String description;
-}
