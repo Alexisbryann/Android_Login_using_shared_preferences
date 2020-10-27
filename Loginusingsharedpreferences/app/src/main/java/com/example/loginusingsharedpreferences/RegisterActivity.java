036d1bc751c663e1a3d0229b4fc3ed0cc011c14a
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-10-27 16:34:57 +0300

    working on login UI beauty

--- Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/RegisterActivity.java
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/RegisterActivity.java
@@ -1,4 +1,15 @@
 package com.example.loginusingsharedpreferences;
 
-public class RegisterActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
 }
