12d2f77635091ff8c519729ad205a7e5be8e38bd
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-10-28 18:13:55 +0300

    working on UI beauty

--- Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
@@ -34,5 +34,13 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/RegisterActivity.java
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginusingsharedpreferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        Button register = findViewById(R.id.button_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
