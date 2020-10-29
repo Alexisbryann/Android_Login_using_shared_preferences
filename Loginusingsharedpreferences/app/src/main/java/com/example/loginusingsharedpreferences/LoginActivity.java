7ebf3a6d25ecd8585de057246bfe6ca62a426acf
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-10-29 22:46:48 +0300

    Login logic working as expected

--- Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
+++ Loginusingsharedpreferences/app/src/main/java/com/example/loginusingsharedpreferences/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,8 +23,12 @@ public class LoginActivity extends AppCompatActivity {
 
     public static  String MyPreferences = "myPrefs";
     private SharedPreferences mSharedPreferences;
-    private final String KEY_USERNAME = "username";
+    private final String USERNAME = "username";
     private final String KEY_PASSWORD = "password";
+    private String mUname1;
+    private String mPword1;
+    private String mUname2;
+    private String mPword2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,11 +57,23 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void validateUser(){
 
-//        mSharedPreferences = getSharedPreferences(MyPreferences,MODE_PRIVATE);
-//        mUname = mSharedPreferences.getString(KEY_USERNAME,"");
-//        mPword = mSharedPreferences.getString(KEY_PASSWORD,"");
+        mSharedPreferences = getSharedPreferences(MyPreferences,MODE_PRIVATE);
 
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+        mUname1 = mUsername.getText().toString();
+        mPword1 = mPassword.getText().toString();
+
+        mUname2 = mSharedPreferences.getString("Username","");
+        mPword2 = mSharedPreferences.getString("Password","");
+
+        if (mUname1.equals(mUname2) && mPword1.equals(mPword2)&&!mUname2.isEmpty()&&!mPword2.isEmpty()){
+            Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            mUsername.setError("Invalid Username");
+            mPassword.setError("Invalid password");
+        }
+        if (mUname1.isEmpty()){mUsername.setError("Enter Username");}
+        if (mPword1.isEmpty()){mPassword.setError("Enter password");}
     }
 }
